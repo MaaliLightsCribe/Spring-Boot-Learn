b735aed174ed529f35b729fa74fef937969b5e33
MUHAMMAD FARHAN <m.farhan@veroke.com>
2023-07-16 01:38:26 +0500

    project setup for sprong boot learn

---
+++ src/test/java/com/uuid/demoSpringBoot/DemoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uuid.demoSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
